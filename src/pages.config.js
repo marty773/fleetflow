ba501aa22edb3e821d3c2a27ec8dcf6326eb4ae3
manny <manny@gazebokits.com>
2026-01-06 17:36:56 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,12 +1,14 @@
 import Dashboard from './pages/Dashboard';
 import Vehicles from './pages/Vehicles';
 import Bills from './pages/Bills';
+import Maintenance from './pages/Maintenance';
 
 
 export const PAGES = {
     "Dashboard": Dashboard,
     "Vehicles": Vehicles,
     "Bills": Bills,
+    "Maintenance": Maintenance,
 }
 
 export const pagesConfig = {
