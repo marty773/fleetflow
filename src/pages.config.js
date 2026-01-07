d2f6bd2e8ab2f9b3a89b470c1a8be348c73ff462
manny <manny@gazebokits.com>
2026-01-07 02:53:53 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,23 +1,13 @@
-import Bills from './pages/Bills';
 import Calendar from './pages/Calendar';
-import Dashboard from './pages/Dashboard';
-import ItemGallery from './pages/ItemGallery';
-import Maintenance from './pages/Maintenance';
-import Vehicles from './pages/Vehicles';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
-    "Bills": Bills,
     "Calendar": Calendar,
-    "Dashboard": Dashboard,
-    "ItemGallery": ItemGallery,
-    "Maintenance": Maintenance,
-    "Vehicles": Vehicles,
 }
 
 export const pagesConfig = {
-    mainPage: "Dashboard",
+    mainPage: "Calendar",
     Pages: PAGES,
     Layout: __Layout,
 };
