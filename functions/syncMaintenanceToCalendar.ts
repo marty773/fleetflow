1d95a8c20cfc95ed6001031f7dc20d64b192e5dd
manny <manny@gazebokits.com>
2026-02-02 18:44:17 +0000

    File changes

--- functions/syncMaintenanceToCalendar.ts
+++ functions/syncMaintenanceToCalendar.ts
@@ -3,12 +3,7 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 Deno.serve(async (req) => {
   try {
     const base44 = createClientFromRequest(req);
-    const user = await base44.auth.me();
-
-    if (!user) {
-      return Response.json({ error: 'Unauthorized' }, { status: 401 });
-    }
-
+    
     const { interval_id } = await req.json();
 
     // Get access token
