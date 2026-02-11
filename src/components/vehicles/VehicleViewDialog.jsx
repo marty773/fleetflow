1d11aa686f5c09cc017500f914f7029353d50c27
manny <manny@gazebokits.com>
2026-02-11 14:47:16 +0000

    File changes

--- src/components/vehicles/VehicleViewDialog.jsx
+++ src/components/vehicles/VehicleViewDialog.jsx
@@ -91,7 +91,7 @@ export default function VehicleViewDialog({ vehicle, open, onOpenChange, onEdit 
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-4xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="sm:max-w-4xl max-h-[90vh] overflow-y-auto relative">
         <DialogHeader>
           <div className="flex items-start justify-between">
             <div>
