40b32a70767581966ba76d6f68aa0e92eeb33f71
manny <manny@gazebokits.com>
2026-02-11 14:21:21 +0000

    File changes

--- src/components/dialogs/MaintenanceRecordDetailDialog.jsx
+++ src/components/dialogs/MaintenanceRecordDetailDialog.jsx
@@ -11,11 +11,11 @@ export default function MaintenanceRecordDetailDialog({ record, vehicles = [], i
 
   return (
     <Dialog open={!!record} onOpenChange={onClose}>
-      <DialogContent className="max-w-3xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="max-w-3xl max-h-[90vh] flex flex-col">
         <DialogHeader>
           <DialogTitle>Maintenance Record Details</DialogTitle>
         </DialogHeader>
-        <div className="space-y-4">
+        <div className="space-y-4 overflow-y-auto flex-1">
           <div className="grid grid-cols-2 gap-4">
             <div>
               <Label className="text-slate-500">Vehicle</Label>
