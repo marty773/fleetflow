968b8a2ad698a897adf223ea552044ccdceccd6f
manny <manny@gazebokits.com>
2026-02-11 12:47:21 +0000

    File changes

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -730,23 +730,15 @@ export default function Items() {
               >
                 Close
               </Button>
-              <Link 
-                to={createPageUrl('Maintenance') + `?edit=${viewingMaintenanceRecord.id}`}
-                className="flex-1"
-                onClick={(e) => {
-                  e.preventDefault();
+              <Button 
+                onClick={() => {
                   setViewingMaintenanceRecord(null);
-                  setTimeout(() => navigate(createPageUrl('Maintenance') + `?edit=${viewingMaintenanceRecord.id}`), 0);
+                  navigate(createPageUrl('Maintenance') + `?edit=${viewingMaintenanceRecord.id}`);
                 }}
+                className="w-full bg-amber-600 hover:bg-amber-700"
               >
-                <Button 
-                  type="button"
-                  className="w-full bg-amber-600 hover:bg-amber-700"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Edit
-                </Button>
-              </Link>
+                Edit
+              </Button>
             </div>
           </DialogContent>
         </Dialog>
