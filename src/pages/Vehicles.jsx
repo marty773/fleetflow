3cfe4fc914defdb180e393c4fa50124a80d4a041
manny <manny@gazebokits.com>
2026-03-12 17:10:27 +0000

    File changes

--- src/pages/Vehicles.jsx
+++ src/pages/Vehicles.jsx
@@ -7,6 +7,7 @@ import { Plus, Trash2, Edit2 } from 'lucide-react';
 import VehicleForm from '../components/vehicles/VehicleForm';
 import VehicleCard from '../components/vehicles/VehicleCard';
 import VehicleViewDialog from '../components/vehicles/VehicleViewDialog';
+import FleetLiveSection from '../components/vehicles/FleetLiveSection';
 import PullToRefresh from '../components/PullToRefresh';
 import PageTransition from '../components/PageTransition';
 import { useCompany } from '../components/CompanyContext';
