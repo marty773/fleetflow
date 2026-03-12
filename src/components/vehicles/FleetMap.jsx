a91c4149ecdad72cc79234a3d1dada9d87cb5628
manny <manny@gazebokits.com>
2026-03-12 17:19:43 +0000

    File changes

--- src/components/vehicles/FleetMap.jsx
+++ src/components/vehicles/FleetMap.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Truck, Navigation } from 'lucide-react';
