f2aefcaf0d47d4a0258c8e6b6b79e16a7b9ac55b
manny <manny@gazebokits.com>
2026-03-13 23:58:12 +0000

    File changes

--- src/components/maintenance/MarkCompleteDialog.jsx
+++ src/components/maintenance/MarkCompleteDialog.jsx
@@ -36,6 +36,9 @@ export default function MarkCompleteDialog({ interval, records, bills, vendors, 
       setNewRecordVendor('');
       setNewRecordNotes('');
       setNewRecordCost('');
+      setAttachBill(false);
+      setBillAmount('');
+      setBillVendor('');
     }
   }, [interval?.id]);
 
