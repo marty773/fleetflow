1aa613f47017ebc152e6d6770aa82871a549fa87
manny <manny@gazebokits.com>
2026-03-20 21:45:45 +0000

    File changes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -31,10 +31,7 @@ export default function SettingsPage() {
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    base44.auth.me().then(u => {
-      console.log('[Settings] user object:', JSON.stringify(u));
-      setUser(u);
-    });
+    base44.auth.me().then(setUser);
   }, []);
 
   useEffect(() => {
