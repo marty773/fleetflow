d23687476def0be7d6b98a8d61413a5d210e72f7
manny <manny@gazebokits.com>
2026-03-23 23:37:21 +0000

    Manual code change

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -252,7 +252,7 @@ export default function Items() {
       </div>
 
       {/* Toolbar */}
-      <div className="flex flex-col sm:flex-row gap-4 mb-8">
+      <div className="flex flex-col sm:flex-row sm:flex-end gap-4 mb-8">
         <div className="relative flex-1 max-w-md">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-slate-400 dark:text-slate-500" />
           <Input
