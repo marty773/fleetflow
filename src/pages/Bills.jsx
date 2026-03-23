930e372fbe28e1be68ce7aa436306888c923fa8e
manny <manny@gazebokits.com>
2026-03-23 23:31:08 +0000

    Manual code change

--- src/pages/Bills.jsx
+++ src/pages/Bills.jsx
@@ -164,7 +164,7 @@ export default function Bills() {
         </div>
 
         {/* Search & Filters */}
-        <div className="flex flex-col sm:flex-row gap-3 mb-4 mt-6">
+        <div className="flex flex-col sm:flex-row sm:items-end gap-3 mb-4 mt-6">
           <div className="relative flex-1">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-slate-400" />
             <Input
